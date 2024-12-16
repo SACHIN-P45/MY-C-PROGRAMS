c7bf63744e45b2580e9722bbceaca058d5a5c3f9
SACHIN-P45 <sachinabi67@gmail.com>
2024-12-16 15:51:14 +0530

    Create Greatest_two_numbers.c

---
+++ Greatest_two_numbers.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;//variable declaration
+    scanf("%d\n",&a);
+    scanf("%d\n",&b);//input statements
+    if(a>b)//condition t0 cheack the greatest number
+        printf("%d is greater",a);//output Statement
+    else
+        printf("%d is greater",b);//output Statement
+    return 0;
+}
