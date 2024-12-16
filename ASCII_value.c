0fb6fd603bde652a5d9fc4d38e5d7af5de5719ab
SACHIN-P45 <sachinabi67@gmail.com>
2024-12-16 15:44:00 +0530

    Create ASCII_value.c

---
+++ ASCII_value.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+    char ch;//character variale declaration
+    scanf("%c",&ch);//input statement
+    printf("%d",(int)ch);//type casting character->integer
+    return 0;
+}
